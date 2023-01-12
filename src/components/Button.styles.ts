c3bd429abda1b27c9df5cacfb376d2575103338c
Carol Chaves <carolina.amaralchaves@gmail.com>
2023-01-12 18:41:48 -0300

    Structure and ESLint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,23 +1,14 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonVariantProps {
-    variant: ButtonVariant
-}
-
-const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green'
+  variant: ButtonVariant
 }
 
 export const ButtonContainer = styled.button<ButtonVariantProps>`
   width: 100px;
   height: 40px;
 
-  ${props => {
-    return css`background-color: ${buttonVariants[props.variant]}`
-  }}
+  background-color: ${(props) => props.theme['green-500']};
 `
