76e1f37d591f0602ba89c75ae0c6dcea25abbde8
Carol Chaves <carolina.amaralchaves@gmail.com>
2023-01-25 15:22:12 -0300

    InterruptedDate Bug fixed

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -75,7 +75,7 @@ export function CyclesContextProvider({
     setCycles((state) =>
       state.map((cycle) => {
         if (cycle.id === activeCycleId) {
-          return { ...cycle, interrupedDate: new Date() }
+          return { ...cycle, interruptedDate: new Date() }
         } else {
           return cycle
         }
